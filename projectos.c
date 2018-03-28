1495cbee6b50c675288de84cfe0854b732e50d59
Keshav9863 <kk250578@gmail.com>
2018-03-28 21:40:46 +0530

    Update projectos.c

--- projectos.c
+++ projectos.c
@@ -1,5 +1,27 @@
-#include<iostream>
-using namespace std;
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+struct nos
+{
+	int range;
+	int marks[range];
+	
+}
+int average;
+int maximum;
+int minimum;
+void *Average_t1()
+{
+	
+}
+void *Maximum_t2()
+{
+	
+}
+void *Minimum_t3()
+{
+
+}
 int main()
 {
 	cout<<"Hello world";
